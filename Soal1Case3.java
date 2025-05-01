f5c453024b406ec1ed1fcc37220cb36706ccda74
Azriel Risywan Attaufiqi <azriel.risywan@pactindo.com>
2025-05-01 19:15:27 +0700

    Initial Commit

---
+++ Soal1Case3.java
@@ -0,0 +1,5 @@
+public class Soal1Case3 {
+    public static void main(String[] args) {
+        System.out.println("Soal 1 Case 3");
+    }
+}
---
+++ Soal1Case4.java
@@ -0,0 +1,5 @@
+public class Soal1Case4 {
+    public static void main(String[] args) {
+        System.out.println("Soal 1 Case 4");
+    }
+}
